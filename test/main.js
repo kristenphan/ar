f17da33a1f84600c3082b0fde0423b675196be19
Kristen Phan <hgiang.phan@yahoo.com.vn>
2022-09-15 18:26:22 +0200

    add files for testing

--- test/main.js
+++ test/main.js
@@ -1,15 +1,15 @@
-import {CSS3DObject} from '../../libs/three.js-r132/examples/jsm/renderers/CSS3DRenderer.js';
-import {mockWithImage} from '../../libs/camera-mock.js';
+import {CSS3DObject} from './CSS3DRenderer.js';
+import {mockWithImage} from './camera-mock.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     // Use mock image for testing
-    mockWithImage('../../assets/mock-videos/course-banner2.png');
+    mockWithImage('./course-banner2.png');
     
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/course-banner.mind',
+      imageTargetSrc: './course-banner.mind',
     });
     const {renderer, cssRenderer, scene, cssScene, camera} = mindarThree;
 
