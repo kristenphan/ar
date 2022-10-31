8da3a70a21b4d45cda7e2b1376c728e150ce8015
Kristen Phan <hgiang.phan@yahoo.com.vn>
2022-10-31 21:53:18 +0100

    move <button> out of <form>. Return button returns to index.html regardless of whether radio button is clicked

--- thesis/threejs-src/water-me.js
+++ thesis/threejs-src/water-me.js
@@ -1,31 +1,24 @@
 // Wait until html finishes loading
 document.addEventListener("DOMContentLoaded", () => {
-  // If button Submit is clicked, add a new watering record to MySQL
+  // If button Submit is clicked, add a new watering record to backend database
   document.getElementById("button-submit").addEventListener("click", () => {
     console.log("water me button submit clicked");
     
-    // Obtain the plant status and write it to MySQL data in a new watering record
+    // Obtain the plant status and write it to a database
     const radioButtonGroup = document.getElementsByName("plant-status");
     const checkedRadio = Array.from(radioButtonGroup).find((radio) => radio.checked);
     if (checkedRadio !== undefined) {
+      alert("radio = ", checkedRadio.value); // TODO: not showing checkRadio.value
       console.log("radio = ", checkedRadio.value);
 
-      // TODO: Call lambda function to write to MySQL
+      // TODO: Call lambda function to write to database
 
       // Return to Main Dashboard
-      /* window.location.href = "./index.html"; // not working */
+      //window.location.href = "./index.html"; // not working
+      //window.location.replace("./index.html"); // not working either
     }
-    else { // No radio button clicked
+    else { // If no radio button clicked
       alert("Please answer the question(s) before clicking 'Submit'.");
     }
-    
   });
-
-  // If button Return is clicked, return to the AR dashboard
-  document.getElementById("button-return").addEventListener("click", () => {
-    console.log("water me button return clicked");
-    // TODO: Return to index.html after Return button is clicked
-    // Currently can return to index.html when no radio button is clicked (see water-me.html)
-    // But when radio button is clicked, cannot return to index.html yet
-  }); 
 });
