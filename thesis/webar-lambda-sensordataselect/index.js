a24a53e938be94debe56ece40e8aa99c560842af
Kristen Phan <hgiang.phan@yahoo.com.vn>
2023-01-03 13:12:51 +0100

    add custom event to pass sensorId in event.body and return a response with sensorValue and timestamp. tested with VS Thunder client

--- thesis/webar-lambda-sensordataselect/index.js
+++ thesis/webar-lambda-sensordataselect/index.js
@@ -1,13 +1,18 @@
 import {DynamoDBClient, QueryCommand} from "@aws-sdk/client-dynamodb";
+let response;
 
 export const handler = async (event) => {
+    // Extract params from event
+    const body = JSON.parse(event.body);
+    const sensorId = body.sensorId;
+    
     // Create a DynamoDBClient which auto marshalls JSON-like params to DynamoDB JSON
     const ddbClient = new DynamoDBClient({region: "eu-central-1"});
     
     const params = {
-        TableName: "webar-ddb-plantId.1.sensordata",
+        TableName: "webar-ddb-sensordata",
         ExpressionAttributeValues: {
-            ":sensorId": {"N": "1"},
+            ":sensorId": {"N": sensorId},
         },
         KeyConditionExpression: "sensorId = :sensorId", // sensorId = partition key
         ScanIndexForward: false, // timestamp = sort key: False = sort from newest to oldest
@@ -17,10 +22,21 @@ export const handler = async (event) => {
     // Run query. Lambda is given IAM role to access Dynamodb table
     try {
         const data = await ddbClient.send(new QueryCommand(params));
+        const sensorValue = data.Items[0].sensorValue["N"]; 
+        const timestamp = data.Items[0].timestamp["N"];
         console.log("Command success!");
         console.log("data.Items = ", data.Items);
-        console.log("data = ", data);
+        response = {
+            "statusCode": 200,
+            "body": JSON.stringify({
+                sensorValue: sensorValue,
+                timestamp: timestamp
+            })
+        }
+        
     } catch (err) {
         console.log("error = ", err);
+        return err;
     }
+    return response;
 };
