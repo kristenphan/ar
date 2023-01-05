a9d4d69a7faa02e2623174b5b3490e4632c097fb
Kristen Phan <hgiang.phan@yahoo.com.vn>
2023-01-05 18:24:45 +0100

    call insertWateringRecord from insertwateringrecord.js

--- thesis/threejs-frontend/src/waterme.js
+++ thesis/threejs-frontend/src/waterme.js
@@ -1,3 +1,8 @@
+import insertWateringRecord from "./insertwateringrecord";
+
+const plantId = 1;
+const LambdaFunctionURLWaterMeInsert = "https://6dixquaejcwbhnoqcxvka4bpjm0bcaun.lambda-url.eu-central-1.on.aws/"; 
+
 // Wait until html finishes loading, 
 // then invoke a lambda function to write a new watering record in backend database
 document.addEventListener("DOMContentLoaded", () => {
@@ -5,15 +10,22 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("button-submit").addEventListener("click", () => {
     console.log("water me button submit clicked");
     
+    // Obtain epoch time when the form is submitted
+    const timeEpoch = Math.floor(Date.now() / 1000);
+
     // Obtain the plant status and write it to a database
     // Radio button "Good" is checked by default
     const radioButtonGroup = document.getElementsByName("plant-status");
     const checkedRadio = Array.from(radioButtonGroup).find((radio) => radio.checked);
     const plantStatus = checkedRadio.value;
 
-    // TODO: Call to lambda to write a new watering record to database
-    const plantId = 1;
-    alert("Recorded html form: plantId = " + plantId + "; plantStatus = " + plantStatus);
+    // Invoke lambda to write a new watering record to database
+    const success = insertWateringRecord(LambdaFunctionURLWaterMeInsert, plantId, timeEpoch, plantStatus);
+    if (success) {
+      alert("Recorded html form: plantId = " + plantId + "; plantStatus = " + plantStatus);
+    } else {
+      alert("Failed to record a new watering event. Try again later.");
+    }
 
     // Return to main dashboard
     window.location.href = "./index.html";
