2d321fde5f7d5ae58f7ca54c64551d1749798dbf
Kristen Phan <hgiang.phan@yahoo.com.vn>
2023-01-06 14:05:07 +0100

    create const REGION and DDBTABLENAME; replace placeholder param for ddb call with actual param

--- thesis/webar-lambda-watermeselect/index.js
+++ thesis/webar-lambda-watermeselect/index.js
@@ -1,14 +1,21 @@
 import {DynamoDBClient, QueryCommand} from "@aws-sdk/client-dynamodb";
 
+const REGION = "eu-central-1";
+const DDBTABLENAME = "webar-ddb-wateringhistory";
+
 export const handler = async (event) => {
+    // Extract params from event
+    const body = JSON.parse(event.body);
+    const plantIdStr = String(body.plantId);
+
     // Create a DynamoDBClient which auto marshalls JSON-like params to DynamoDB JSON
-    const ddbClient = new DynamoDBClient({region: "eu-central-1"});
+    const ddbClient = new DynamoDBClient({region: REGION});
     
     const params = {
-        TableName: "webar-ddb-wateringhistory",
+        TableName: DDBTABLENAME,
         KeyConditionExpression: "plantId = :plantId",
         ExpressionAttributeValues: {
-            ":plantId": {"N": "1"},
+            ":plantId": {"N": plantIdStr},
         },
         ScanIndexForward: false, // Sort by sort key "timestamp": False = newest to oldest
         Limit: 3, // Show 3 items
@@ -19,7 +26,9 @@ export const handler = async (event) => {
         const data = await ddbClient.send(new QueryCommand(params));
         console.log("Command success!");
         console.log("data.Items = ", data.Items);
+        return data.Items;
     } catch (err) {
         console.log("error = ", err);
+        return err;
     }
 };
